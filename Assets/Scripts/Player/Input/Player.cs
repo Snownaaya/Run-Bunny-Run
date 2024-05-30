efef2b6b50723f6d0c320af3d66ed56586316fb8
Snownaaya <71766716+Snownaaya@users.noreply.github.com>
2024-05-30 12:57:23 +0900

    Changed control to a new input system.

---
+++ Assets/Scripts/Player/Input/Player.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _jumpForce = 10.0f;
+
+    // private PlayerCollisionHandler _playerCollision;
+
+    private PlayerInput _playerInput;
+    private Rigidbody _rigidbody;
+
+    private Vector2 _moveDirection;
+    private bool _jump = false;
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInput(); 
+        _rigidbody = GetComponent<Rigidbody>();
+
+        _playerInput.Player.Move.performed += OnMove;
+        _playerInput.Player.Jump.performed += OnJump;
+        _playerInput.Player.Jump.canceled += OnJump;
+    }
+
+    private void Update()
+    {
+        Move();
+
+        if (_jump)
+        {
+            Jump();
+            _jump = false;
+        }
+    }
+
+    private void OnEnable() => _playerInput.Enable();
+
+    private void OnDisable() => _playerInput.Disable();
+
+    private void OnMove(InputAction.CallbackContext context) => _moveDirection = context.action.ReadValue<Vector2>();
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            _jump = true;
+    }
+
+    private void Move()
+    {
+        if (_moveDirection.sqrMagnitude < 0.1f)
+            return;
+
+        float scaledMoveSpeed = _speed * Time.deltaTime;
+        Vector3 offset = new Vector3(_moveDirection.x, 0f, _moveDirection.y) * scaledMoveSpeed;
+
+        _rigidbody.MovePosition(_rigidbody.position + offset);
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    //private void OnEnable() => _playerCollision.CollisionDetected += ProcessCollision;
+
+    //private void OnDisable() => _playerCollision.CollisionDetected -= ProcessCollision; 
+    //private void ProcessCollision(IInteractable interactable)
+    //{
+    //    if (interactable is GameOverZone)
+    //        GameOver?.Invoke();
+    //}
+}
